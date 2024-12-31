e3407d3b2de78e7b419e8cabe7d36b9288dc8016
Yen Kuo <kuoyen5@gmail.com>
2024-12-31 18:28:18 +1030

    swap out const skill list and use the data from api

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useSpring, animated } from "@react-spring/web";
 import { useQuery } from "@tanstack/react-query";
 import { API_URL } from "@/utils/config";
@@ -29,154 +29,21 @@ interface Experience {
   experienceItemDetails: ExperienceItemDetail[];
 }
 
-const skills = [
-  {
-    category: "Frontend - WEB",
-    items: [
-      {
-        title: "React",
-        icon: "SiReact",
-        color: "#61DAFB",
-      },
-      {
-        title: "Next.js",
-        icon: "SiNextdotjs",
-        color: "#000000",
-      },
-      {
-        title: "Tailwind CSS",
-        icon: "SiTailwindcss",
-        color: "#06B6D4",
-      },
-      {
-        title: "Bootstrap",
-        icon: "SiBootstrap",
-        color: "#7952B3",
-      },
-      {
-        title: "Redux",
-        icon: "SiRedux",
-        color: "#764ABC",
-      },
-    ],
-  },
-  {
-    category: "Frontend - MOBILE",
-    items: [
-      {
-        title: ".NET MAUI",
-        icon: "SiDotnet",
-        color: "#1e40af",
-      },
-      {
-        title: "Xamarin.Android",
-        icon: "SiAndroid",
-        color: "#A4C639",
-      },
+interface SkillCategory {
+  id: number;
+  name: string;
+  order: number;
+  skills: Skill[];
+}
 
-      {
-        title: "React Native",
-        icon: "SiReact",
-        color: "#61DAFB",
-      },
-      {
-        title: "Flutter",
-        icon: "SiFlutter",
-        color: "#02569B",
-      },
-      {
-        title: "Swift UI",
-        icon: "SiSwift",
-        color: "#FA7343",
-      },
-    ],
-  },
-  {
-    category: "Backend",
-    items: [
-      {
-        title: ".NET Web API",
-        icon: "SiDotnet",
-        color: "#1e40af",
-      },
-      {
-        title: "Django",
-        icon: "SiDjango",
-        color: "#092E20",
-      },
-      {
-        title: "PostgreSQL",
-        icon: "SiPostgresql",
-        color: "#336791",
-      },
-      {
-        title: "MSSQL",
-        icon: "DiMsqlServer",
-        color: "#B71C1C",
-      },
-      {
-        title: "Celery",
-        icon: "SiCelery",
-        color: "#37814A",
-      },
-      {
-        title: "Selenium",
-        icon: "SiSelenium",
-        color: "#43B02A",
-      },
-    ],
-  },
-  {
-    category: "DevOps - (Others)",
-    items: [
-      {
-        title: "Kubernetes",
-        icon: "SiKubernetes",
-        color: "#326CE5",
-      },
-      {
-        title: "Microsoft Azure",
-        icon: "SiMicrosoftazure",
-        color: "#0078D4",
-      },
-      {
-        title: "Amazon AWS",
-        icon: "SiAmazonaws",
-        color: "#232F3E",
-      },
-      {
-        title: "Docker",
-        icon: "SiDocker",
-        color: "#2496ED",
-      },
-      {
-        title: "Git",
-        icon: "SiGit",
-        color: "#F14E32",
-      },
-      {
-        title: "Jira",
-        icon: "SiJira",
-        color: "#0052CC",
-      },
-      {
-        title: "Azure DevOps",
-        icon: "SiAzuredevops",
-        color: "#0078D7",
-      },
-      {
-        title: "Terraform",
-        icon: "SiTerraform",
-        color: "#623CE4",
-      },
-      {
-        title: "Dynamics 365 - Business Central",
-        icon: "SiDynamics365",
-        color: "#002050",
-      },
-    ],
-  },
-];
+interface Skill {
+  id: number;
+  title: string;
+  icon: string;
+  color: string;
+  order: number;
+  categoryId: number;
+}
 
 // Format date string to "YYYY-MM"
 const formatDate = (dateString: string): string => {
@@ -191,11 +58,17 @@ const formatDate = (dateString: string): string => {
 
 function ResumePage() {
   // The state to store the fetched data
-  const [workExperience, setWorkExperience] = useState<Experience[]>([]);
+  const [workExperiences, setWorkExperiences] = useState<Experience[]>([]);
   const [educations, setEducations] = useState<Experience[]>([]);
+  const [skillCategories, setSkillCategories] = useState<SkillCategory[]>([]);
 
   // Fetch the data by using react-query
-  const { isPending, isError, data, error } = useQuery({
+  const {
+    isPending: isExperiencesPending,
+    isError: isExperiencesError,
+    data: experiencesData,
+    error: experiencesError,
+  } = useQuery({
     queryKey: ["experiences"],
     queryFn: async () => {
       const response = await fetch(`${API_URL}/api/experiences`);
@@ -203,9 +76,33 @@ function ResumePage() {
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }
-      const data = await response.json();
 
-      const workExperiences = data
+      return response.json();
+    },
+  });
+
+  const {
+    isPending: isSkillsPending,
+    isError: isSkillsError,
+    data: skillCategoriesData,
+    error: skillsError,
+  } = useQuery({
+    queryKey: ["skillCategories"],
+    queryFn: async () => {
+      const response = await fetch(`${API_URL}/api/skill-categories`);
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      return response.json();
+    },
+  });
+
+  // Update the state when the data is fetched
+  useEffect(() => {
+    if (experiencesData) {
+      const workExperiences = experiencesData
         .filter((item: Experience) => item.experienceItemType === "Work")
         .map((item: Experience) => ({
           ...item,
@@ -213,12 +110,12 @@ function ResumePage() {
           endDate: item.endDate ? formatDate(item.endDate) : null,
         }))
         .sort(
-          (fristItem: Experience, secondItem: Experience) =>
+          (firstItem: Experience, secondItem: Experience) =>
             new Date(secondItem.startDate).getTime() -
-            new Date(fristItem.startDate).getTime(),
+            new Date(firstItem.startDate).getTime(),
         );
 
-      const educationExperiences = data
+      const educationExperiences = experiencesData
         .filter((item: Experience) => item.experienceItemType === "Education")
         .map((item: Experience) => ({
           ...item,
@@ -226,129 +123,143 @@ function ResumePage() {
           endDate: item.endDate ? formatDate(item.endDate) : null,
         }))
         .sort(
-          (fristItem: Experience, secondItem: Experience) =>
+          (firstItem: Experience, secondItem: Experience) =>
             new Date(secondItem.startDate).getTime() -
-            new Date(fristItem.startDate).getTime(),
+            new Date(firstItem.startDate).getTime(),
         );
 
-      setWorkExperience(workExperiences);
+      setWorkExperiences(workExperiences);
       setEducations(educationExperiences);
+    }
+  }, [experiencesData]);
 
-      return data;
-    },
-  });
+  useEffect(() => {
+    if (skillCategoriesData) {
+      // Handle skill categories data here and update state
+      setSkillCategories(
+        skillCategoriesData.sort(
+          (a: SkillCategory, b: SkillCategory) => a.order - b.order,
+        ),
+      ); // Assuming you have a setSkillCategories function
+    }
+  }, [skillCategoriesData]);
 
   // Fade in animation
   const fadeInAnimation = useSpring({
-    opacity: isPending ? 0 : 1,
-    transform: isPending ? "translateY(20px)" : "translateY(0px)",
+    opacity: isExperiencesPending ? 0 : 1,
+    transform: isExperiencesPending ? "translateY(20px)" : "translateY(0px)",
     config: { duration: 500 },
   });
 
-  useScript(`https://cdn.credly.com/assets/utilities/embed.js`, data);
+  useScript(
+    `https://cdn.credly.com/assets/utilities/embed.js`,
+    experiencesData,
+  );
 
   return (
-    <>
-      <PageWrapper disappearOnMobile={true} noPadding={true}>
-        <div className="flex items-center justify-between py-4 mb-4 border-b border-gray-300 border-dotted">
-          <h1 className="text-3xl font-bold">Resume</h1>
-          <a
-            target="_blank"
-            href="https://yenwebsitebucket83ca8-dev.s3.ap-southeast-2.amazonaws.com/public/files/Resume.pdf"
-            className="bg-amber-500 text-white font-bold py-2 px-2 lg:px-4 rounded-full shadow-lg duration-500 hover:bg-primary hover:shadow-2xl hover:scale-105"
-          >
-            Download
-          </a>
-        </div>
-        {(() => {
-          if (isPending) {
-            return (
-              <LoadingSpinner>
-                Good things take time... almost there! 🚀
-              </LoadingSpinner>
-            );
-          } else if (isError) {
-            const errorMessages = error?.message ?? "An error occurred";
-            return <ErrorMessage error={error} />;
-          } else {
-            return (
-              <animated.div style={fadeInAnimation}>
-                <h2 className="text-xl font-bold mb-2">Work Experience</h2>
-                {workExperience.map((job, index) => (
-                  <div key={index} className="mb-4 space-y-2 > * ml-4">
-                    <div className="grid grid-cols-1 lg:grid-cols-3 items-center text-indigo-300 lg:text-indigo-900">
-                      <h3 className="text-lg font-bold">{job.title}</h3>
-                      <p className="text-left lg:text-center">
-                        {job.country}, {job.city}
-                      </p>
-                      <p className="text-left lg:text-right">
-                        {formatDate(job.startDate)} -{" "}
-                        {job.isCurrent
-                          ? "Present"
-                          : formatDate(job.endDate ?? "")}
-                      </p>
-                    </div>
-                    <div className="font-bold">{job.position}</div>
-                    <ul className="break-words">
-                      {job.experienceItemDetails.map((detail, index) => (
-                        <li key={index}>- {detail.description}</li>
-                      ))}
-                    </ul>
+    <PageWrapper disappearOnMobile={true} noPadding={true}>
+      <div className="flex items-center justify-between py-4 mb-4 border-b border-gray-300 border-dotted">
+        <h1 className="text-3xl font-bold">Resume</h1>
+        <a
+          target="_blank"
+          href="https://yenwebsitebucket83ca8-dev.s3.ap-southeast-2.amazonaws.com/public/files/Resume.pdf"
+          className="bg-amber-500 text-white font-bold py-2 px-2 lg:px-4 rounded-full shadow-lg duration-500 hover:bg-primary hover:shadow-2xl hover:scale-105"
+        >
+          Download
+        </a>
+      </div>
+      {(() => {
+        if (isExperiencesPending || isSkillsPending) {
+          return (
+            <LoadingSpinner>
+              Good things take time... almost there! 🚀
+            </LoadingSpinner>
+          );
+        } else if (isExperiencesError || isSkillsError) {
+          if (isSkillsError) {
+            return <ErrorMessage error={skillsError} />;
+          }
+          return <ErrorMessage error={experiencesError} />;
+        } else {
+          return (
+            <animated.div style={fadeInAnimation}>
+              <h2 className="text-xl font-bold mb-2">Work Experience</h2>
+              {workExperiences.map((job, index) => (
+                <div key={index} className="mb-4 space-y-2 > * ml-4">
+                  <div className="grid grid-cols-1 lg:grid-cols-3 items-center text-indigo-300 lg:text-indigo-900">
+                    <h3 className="text-lg font-bold">{job.title}</h3>
+                    <p className="text-left lg:text-center">
+                      {job.country}, {job.city}
+                    </p>
+                    <p className="text-left lg:text-right">
+                      {formatDate(job.startDate)} -{" "}
+                      {job.isCurrent
+                        ? "Present"
+                        : formatDate(job.endDate ?? "")}
+                    </p>
                   </div>
-                ))}
-                <h2 className="text-xl font-bold mb-2">Education</h2>
-                {educations.map((education, index) => (
-                  <div key={index} className="mb-4 space-y-2 > * ml-4">
-                    <div className="grid grid-cols-1 lg:grid-cols-3 items-center text-indigo-300 lg:text-indigo-900">
-                      <h3 className="text-lg font-bold">{education.title}</h3>
-                      <p className="text-left lg:text-center">
-                        {education.country}, {education.city}
-                      </p>
-                      <p className="text-left lg:text-right">
-                        {formatDate(education.startDate)} -{" "}
-                        {education.isCurrent
-                          ? "Present"
-                          : formatDate(education.endDate ?? "")}
-                      </p>
-                    </div>
-                    <div className="font-bold">{education.position}</div>
+                  <div className="font-bold">{job.position}</div>
+                  <ul className="break-words">
+                    {job.experienceItemDetails.map((detail, index) => (
+                      <li key={index}>- {detail.description}</li>
+                    ))}
+                  </ul>
+                </div>
+              ))}
+              <h2 className="text-xl font-bold mb-2">Education</h2>
+              {educations.map((education, index) => (
+                <div key={index} className="mb-4 space-y-2 > * ml-4">
+                  <div className="grid grid-cols-1 lg:grid-cols-3 items-center text-indigo-300 lg:text-indigo-900">
+                    <h3 className="text-lg font-bold">{education.title}</h3>
+                    <p className="text-left lg:text-center">
+                      {education.country}, {education.city}
+                    </p>
+                    <p className="text-left lg:text-right">
+                      {formatDate(education.startDate)} -{" "}
+                      {education.isCurrent
+                        ? "Present"
+                        : formatDate(education.endDate ?? "")}
+                    </p>
                   </div>
-                ))}
-                <h2 className="text-xl font-bold">Skills</h2>
-                {skills.map((skill, index) => (
-                  <div
-                    key={index}
-                    className="my-2 p-2 lg:p-6 bg-white/50 rounded-lg shadow-lg space-y-4 > *"
-                  >
-                    <div className="flex flex-col lg:flex-row lg:justify-between lg:items-center ">
-                      <h3 className="text-lg font-bold">{skill.category}</h3>
-                    </div>
-                    <div className="flex flex-wrap">
-                      {skill.items.map((item, index) => (
+                  <div className="font-bold">{education.position}</div>
+                </div>
+              ))}
+              <h2 className="text-xl font-bold">Skills</h2>
+              {skillCategories.sort().map((skillCategory) => (
+                <div
+                  key={`skill-category-${skillCategory.id}`}
+                  className="my-2 p-2 lg:p-6 bg-white/50 rounded-lg shadow-lg space-y-4 > *"
+                >
+                  <div className="flex flex-col lg:flex-row lg:justify-between lg:items-center ">
+                    <h3 className="text-lg font-bold">{skillCategory.name}</h3>
+                  </div>
+                  <div className="flex flex-wrap">
+                    {skillCategory.skills
+                      .sort((a: Skill, b: Skill) => a.order - b.order)
+                      .map((skill) => (
                         <div
-                          key={index}
+                          key={`skill-${skill.id}`}
                           className="flex items-center space-x-1 mx-2 inline-flex items-center rounded-md bg-gray-50 px-2 py-1 mb-1 text-xs font-medium text-gray-600 ring-1 ring-inset ring-gray-500/10"
                         >
-                          <IconRenderer icon={item.icon} color={item.color} />
-                          <span>{item.title}</span>
+                          <IconRenderer icon={skill.icon} color={skill.color} />
+                          <span>{skill.title}</span>
                         </div>
                       ))}
-                    </div>
                   </div>
-                ))}
-                <h2 className="text-xl font-bold">Certificate</h2>
-                <div
-                  data-iframe-width="300"
-                  data-iframe-height="300"
-                  data-share-badge-id="948aa351-269b-46d7-a63e-66c3e33f3ee9"
-                  data-share-badge-host="https://www.credly.com"
-                />
-              </animated.div>
-            );
-          }
-        })()}
-      </PageWrapper>
-    </>
+                </div>
+              ))}
+              <h2 className="text-xl font-bold">Certificate</h2>
+              <div
+                data-iframe-width="300"
+                data-iframe-height="300"
+                data-share-badge-id="948aa351-269b-46d7-a63e-66c3e33f3ee9"
+                data-share-badge-host="https://www.credly.com"
+              />
+            </animated.div>
+          );
+        }
+      })()}
+    </PageWrapper>
   );
 }
 
